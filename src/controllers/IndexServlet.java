d7ae509af3a035f26a8a90f566a5694ba9bd5a2d
keiko sagayama <blackvalley45@yahoo.ne.jp>
2018-11-02 13:05:56 +0900

    Add pom.xml

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,11 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -13,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +34,41 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    EntityManager em = DBUtil.createEntityManager();
+
+	    int page = 1;
+	    try {
+	        page = Integer.parseInt(request.getParameter("page"));
+	    } catch(NumberFormatException e) {}
+
+
+
+	    List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+
+	            .setFirstResult(15 * (page - 1))
+                .setMaxResults(15)
+	            .getResultList();
+
+	    long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                .getSingleResult();
+
+
+	    em.close();
+
+	    request.setAttribute("tasks", tasks);
+
+	    request.setAttribute("tasks_count", tasks_count);
+	    request.setAttribute("page", page);
+
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+	    rd.forward(request, response);
 	}
 
-}
+
+
+	}
+
+
+
+
+
